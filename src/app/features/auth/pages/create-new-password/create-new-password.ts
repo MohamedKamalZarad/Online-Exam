b4fd6e9ddc2a55a29891f588a83de8771063acb0
unknown <Mohamedkamalzrd@gmail.com>
2025-11-09 20:01:52 +0200

    add pages

---
+++ src/app/features/auth/pages/create-new-password/create-new-password.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-new-password',
+  imports: [],
+  templateUrl: './create-new-password.html',
+  styleUrl: './create-new-password.scss',
+})
+export class CreateNewPassword {
+
+}
