02619305a5dcb1d045e7af0fdcfbc2fc2b677f2b
unknown <Mohamedkamalzrd@gmail.com>
2025-11-09 17:45:35 +0200

    auth

---
+++ src/app/features/auth/pages/verify-otp/verify-otp.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: 'app-verify-otp',
+  imports: [RouterLink],
+  templateUrl: './verify-otp.html',
+  styleUrl: './verify-otp.scss',
+})
+export class VerifyOtp {
+
+}
